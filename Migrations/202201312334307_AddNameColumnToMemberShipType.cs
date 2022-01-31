fc532f90b6f6e6436d1de83e1b4ca63b417e16ea
Bryan alvarez <bryan.alvarez@gmail.com>
2022-01-31 15:40:17 -0800

    Added Name column to MembershipType table and updated values.

---
+++ Migrations/202201312334307_AddNameColumnToMemberShipType.cs
@@ -0,0 +1,17 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToMemberShipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("ALTER TABLE MembershipTypes ADD Name varchar(255)");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

64c87270d6dcd134470bc6615d2a55f0c03b1f97
Bryan alvarez <bryan.alvarez@gmail.com>
2022-01-31 15:29:22 -0800

    Add project files.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Custom routes, must come before our default route
+
+            routes.MapMvcAttributeRoutes();
+
+            /* This is the old way of doing attribute routing 
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action="ByReleaseDate"},
+                // The @ sign just makes sure the backslash is treated as text and not an escape character.
+                new { year = @"2021|2022", month = @"\d{2}" }
+            );
+            */
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Vidly.Models
+{
+    public class Movie
+    {
+        // This is a POCO
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    // /movies/random
+}

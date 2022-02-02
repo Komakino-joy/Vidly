5d960afd13feec81d70d7b8450d81ab9fce9e57c
Bryan alvarez <bryan.alvarez1251@gmail.com>
2022-02-01 21:38:44 -0800

    Added server side form validation.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,7 +7,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -20,6 +20,7 @@ namespace Vidly.Models
         public byte MemberShipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
     }
 }
